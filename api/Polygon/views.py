028ab45f87b2c9148cf964839b59481b66410d67
TightLinesOfCode <jdalmon316@gmail.com>
2021-09-23 14:19:00 -0700

    Initial Commit

---
+++ api/Polygon/views.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/")
+def list_data():
+    return {"working polygon, need data"}
--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from api.Polygon import views as  polygon_views
 
+app = FastAPI()
+app.inclue_router(polygon_views.router, prefix="/polygon")
 @app.get("/health")
 def health():
     return {"status":"ok"}
