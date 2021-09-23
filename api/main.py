3231a63ddd3060a48c816e31a2bafef4823f3710
TightLinesOfCode <jdalmon316@gmail.com>
2021-09-23 14:19:49 -0700

    Initial Commit

--- api/main.py
+++ api/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from api.Polygon import views as  polygon_views
 
 app = FastAPI()
-app.inclue_router(polygon_views.router, prefix="/polygon")
+app.include_router(polygon_views.router, prefix="/polygon")
 @app.get("/health")
 def health():
     return {"status":"ok"}
