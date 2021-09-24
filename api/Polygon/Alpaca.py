c7720d0913d49780f546b4bfc658c906a703615c
TightLinesOfCode <jdalmon316@gmail.com>
2021-09-24 12:34:19 -0700

    Initial Commit

--- api/Polygon/Alpaca.py
+++ api/Polygon/Alpaca.py
@@ -11,7 +11,7 @@ async def get_alpaca_data():
     #async with httpx.AsyncClient() as client:
         url = 'https://paper-api.alpaca.markets/v2/account'
         headers = {'APCA-API-KEY-ID': 'PKQO6B0V53BM8BRO3FHL', 'APCA-API-SECRET-KEY': '5Uwl32ELftgVw40RKnxpQvvu3dN6nprg4mI8nzkD'}
-        response = await httpx.get(url, headers=headers)
+        response = httpx.get(url, headers=headers)
 
         #response = "suckshere"
         _load_alpacadata(response.json())
