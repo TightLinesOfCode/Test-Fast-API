fd60a26d133ebcbf57d17cd0334c2d9f2e248427
TightLinesOfCode <jdalmon316@gmail.com>
2021-09-23 20:37:13 -0700

    Initial Commit

--- api/Polygon/data.py
+++ api/Polygon/data.py
@@ -10,9 +10,9 @@ def _load_polygondata(data: str):
 
 async def get_polygon_data(path: str):
     async with httpx.AsyncClient() as client:
-    response = await client.get(path)
-    _load_polygondata(response.json())
-    #print(response.text)
+        response = await client.get(path)
+        _load_polygondata(response.json())
 
-    #_load_polygondata("works")
+        #print(response.text)
+        #_load_polygondata("works")
 
