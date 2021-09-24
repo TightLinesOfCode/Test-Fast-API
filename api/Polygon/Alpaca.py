71833ed40be9019c5c1d2bbbd40bb296865ba059
TightLinesOfCode <jdalmon316@gmail.com>
2021-09-24 12:43:37 -0700

    Initial Commit

--- api/Polygon/Alpaca.py
+++ api/Polygon/Alpaca.py
@@ -19,9 +19,10 @@ async def get_alpaca_data():
 
 def market_order_aapl():
     #async with httpx.AsyncClient() as client:
-         url = 'https://paper-api.alpaca.markets/v2/orders?symbol=AAPL&qty=1&side=buy&type=market'
+         url = 'https://paper-api.alpaca.markets/v2/orders'
          headers = {'APCA-API-KEY-ID': 'PKQO6B0V53BM8BRO3FHL', 'APCA-API-SECRET-KEY': '5Uwl32ELftgVw40RKnxpQvvu3dN6nprg4mI8nzkD'}
-         response = httpx.put(url, headers=headers)
+         params = {'symbol:AAPL', 'qty:1', 'side:buy', 'type:market'}
+         response = httpx.put(url, headers=headers, params=params)
 
          ORDERDATA[0] = response.json()
 
