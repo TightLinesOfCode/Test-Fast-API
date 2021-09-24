a194e0e4362eff0d36814111c8a1f239c9daa684
TightLinesOfCode <jdalmon316@gmail.com>
2021-09-24 09:50:34 -0700

    Initial Commit

--- noxfile.py
+++ noxfile.py
@@ -6,20 +6,17 @@ def dev(session):
     """For creating a development virtual environment. Handy for setting interpreter in
     IDE.
     """
-    session.install("-r", "test-requirements.txt")
-
+    session.install("-e", "test-requirements.txt")
 
 @nox.session(python="3.8", reuse_venv=True)
 def lint(session):
     session.install("black")
     session.run("black", "api")
 
-
 @nox.session(python="3.8", reuse_venv=True)
 def check(session):
     session.install("flake8")
     session.run("flake8", "api")
-
 
 @nox.session(python="3.8", reuse_venv=True)
 def test(session):
